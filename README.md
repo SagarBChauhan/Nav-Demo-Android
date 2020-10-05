f6208fd2d2a34ecbca9ebcb649a8a985cb006c37
sagar chauhan <chauhansagar373@gmail.com>
2020-10-05 10:30:04 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Nav-Demo-Android" 
---
+++ app/src/main/java/com/example/navdemo/Menu2.java
@@ -0,0 +1,28 @@
+package com.example.navdemo;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Menu2 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //returning our layout file
+        //change R.layout.yourlayoutfilename for each of your fragments
+        return inflater.inflate(R.layout.fragment_menu_2, container, false);
+    }
+
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //you can set the title for your toolbar here for different fragments different titles
+        getActivity().setTitle("Menu 1");
+    }
+}
